a238f111c4e9b6812804d4da3fcb8abcff6ac40e
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-07-15 22:06:51 +0300

    feat: add services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,112 @@
 import React from 'react';
 import './services.css'
+import {BiCheck} from "react-icons/bi";
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>Что я предлагаю</h5>
+      <h2>Услуги</h2>
+
+      <div className="container services__container">
+        <article className="services">
+          <div className="services__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="services__list">
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+          </ul>
+        </article>
+      {/*  End UI/UX */}
+        <article className="services">
+          <div className="services__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="services__list">
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+          </ul>
+        </article>
+      {/*  End UI/UX */}
+        <article className="services">
+          <div className="services__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="services__list">
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+            <li>
+              <BiCheck className="services__list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur</p>
+            </li>
+          </ul>
+        </article>
+        {/*  End UI/UX */}
+      </div>
+    </section>
   )
 }
 
